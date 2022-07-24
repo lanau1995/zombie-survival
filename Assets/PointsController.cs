1b2bb107bd5a6f03b72049c3cd57c0b232fe2f60
lanau1995 <lanaufranciscoupwork@gmail.com>
2022-07-24 19:59:12 -0400

    Imported TMPro. Created player HUD canvas that displays player's points.

--- Assets/PointsController.cs
+++ Assets/PointsController.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PointsController : MonoBehaviour
 {
     [SerializeField] private float startingPoints = 500f;
     [SerializeField] private float points;
 
-
+    [SerializeField] private TextMeshProUGUI pointsText;
 
     void Start()
     {
         points = startingPoints;
+        pointsText.text = points.ToString();
     }
 
     public float Points
@@ -23,6 +25,7 @@ public class PointsController : MonoBehaviour
         set
         {
             points = value;
+            pointsText.text = points.ToString();
             print("POINTS UPDATED TO: " + points);
         }
     }
